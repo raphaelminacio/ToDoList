8f846a86ec4311d8c167311a0a6fd38053787189
Raphael  Menezes <raphaelmenezes@Raphaels-iMac.local>
2019-08-13 15:39:32 -0400

    Initial Commit

---
+++ date.js
@@ -0,0 +1,31 @@
+//jshint esversion:6
+/*
+exports.getDate = getDate;
+
+function getDate() {
+
+  const today = new Date();
+
+  const options = {
+    weekday: "long",
+    month: "long",
+    day: "numeric"
+  };
+
+  return today.toLocaleDateString("en-US", options);
+
+}
+
+exports.getDay = getDay;
+
+function getDay() {
+
+  const today = new Date();
+
+  const options = {
+    weekday: "long"
+  };
+
+  return today.toLocaleDateString("en-US", options);
+
+} */
